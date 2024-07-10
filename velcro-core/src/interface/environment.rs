f89d7e9449972b1d22688ab679042ef09f502f0a
梁渝 <296270991@qq.com>
2024-07-10 14:45:15 +0800

    ...

--- velcro-core/src/interface/environment.rs
+++ velcro-core/src/interface/environment.rs
@@ -2,7 +2,9 @@
 #![allow(clippy::many_single_char_names)]
 
 
-use std::{any::{Any, TypeId}, collections, fmt::Error};
+use std::{any::{Any, TypeId}, collections::HashMap, fmt::Error, ptr::NonNull, result, sync::Mutex};
+use std::sync::Arc;
+use std::rc::Rc;
 use crate::parallel;
 
 pub enum States {
@@ -12,6 +14,7 @@ pub enum States {
     NotFound,
     OutOfMemory,
 }
+
 
 pub struct EnvironmentVariableResult<T> {
     pub state: States,
@@ -27,22 +30,85 @@ impl<T> EnvironmentVariableResult<T> {
     }
 }
 
-pub trait EnvironmentInterface: 'static {
-   fn attach_fallback(source_environment: Box<&EnvironmentInterface>);
-   fn get_fallback() -> Option<Box<&EnvironmentInterface>>;
+pub trait EnvironmentInterface {
+    fn get_variable(&mut self, uid: &u32) -> EnvironmentVariableResult<Option<&Arc<dyn Any>>>;
 }
 
-struct EnvironmentVariableHolderBase {
+pub struct Environment<T: EnvironmentInterface>  {
+    _num_attached: u32,
+    _fallback:  Option<NonNull<T>>,
+    _variables: HashMap<u32, Box<dyn Any>>,
+    _mutex:     Rc<Mutex<u32>>
+}
+
+impl<T: EnvironmentInterface>  EnvironmentInterface for Environment<T> {
+    /*pub fn new() -> Self {
+        Environment {
+            _num_attached: 0,
+            _fallback: None,
+            _variables: HashMap::new(),
+            _mutex: Rc::new(Mutex::new(0))
+        }
+    }*/
+
+
+    /*pub fn remove_variable(&mut self, uid: &u32) -> EnvironmentVariableResult<Option<Arc<dyn Any>>> {
+        let _locker = *self._mutex.lock().unwrap();
+        let result = self._variables.remove(uid);
+        if result.is_none() {
+            return EnvironmentVariableResult {
+                state: States::NotFound,
+                variable: None,
+            }
+        }
+
+        return EnvironmentVariableResult {
+            state: States::Removed,
+            variable: result,
+        }
+    }*/
+
+    fn get_variable(&mut self, uid: &u32) -> EnvironmentVariableResult<Option<&Arc<dyn Any>>>{
+        let result = self._variables.get(uid);
+        match result {
+            None => {
+                match self._fallback {
+                    None => return EnvironmentVariableResult {
+                        state: States::NotFound,
+                        variable: None,
+                    },
+                    Some(fallback) => {
+                        return unsafe {
+                         (*fallback.as_ptr()).get_variable(uid) };
+                    }
+                }
+            },
+            Some(res) => {
+
+            }
+        }
+
+    }
+}
+
+
+
+/*pub trait EnvironmentInterface: 'static {
+   fn attach_fallback(source_environment: Option<Arc<EnvironmentInterface>>);
+   fn get_fallback() -> Option<Arc<EnvironmentInterface>>;
+}*/
+
+/*struct EnvironmentVariableHolderBase {
     _guid: u32,
     _use_count: int32,
     _mutex: parallel::SpinMutex,
 }
 
-/*pub struct EnvironmentVariable {
+pub struct EnvironmentVariable {
 
-}*/
+}
 
-/*pub trait EnvironmentInterface {
+pub trait EnvironmentInterface {
     type Item;
 
     fn attach_fallback(&self, source_environment: Box<dyn EnvironmentInterface<Item = Self::Item>> );
