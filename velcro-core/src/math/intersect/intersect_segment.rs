b59fdbf3022f1cf6355661c139c5fc2bb1cc9365
冷帆520 <11615645+cold-sail-520@user.noreply.gitee.com>
2024-09-11 16:06:13 +0800

    修改Vector4

--- velcro-core/src/math/intersect/intersect_segment.rs
+++ velcro-core/src/math/intersect/intersect_segment.rs
@@ -1,27 +1,90 @@
 #![warn(clip::pedantic)]
 #![allow(clip::many_single_char_names)]
 
+use std::ptr;
+
+use crate::math::vector3::Vector3;
 
 pub struct SegmentTriangleHitTester{
+    _p:Vector3,
+    _pq:Vector3,
 
+    _kx:i32,
+    _ky:i32,
+    _kz:i32,
+
+    _sx:f32,
+    _sy:f32,
+    _sz:f32
 }
 impl SegmentTriangleHitTester{
-    class SegmentTriangleHitTester
-    {
-    public:
-    //! Construct a hit tester for segment pq that can be used to compare against multiple triangles.
-    //! @param p Segment start point.
-    //! @param q Segment end point.
-    SegmentTriangleHitTester(const AZ::Vector3& p, const AZ::Vector3& q);
 
-    //! For the given segment pq and a triangle abc (CCW), returns whether the segment intersects the
-    //! triangle and if so, also returns the time of intersection along the segment and the triangle normal.
-    //! @param a Triangle vertex 1.
-    //! @param b Triangle vertex 2.
-    //! @param c Triangle vertex 3.
-    //! @param normal Triangle normal at the intersection point if segment intersects triangle.
-    //! @param t Time of intersection along the segment [0.0 (p), 1.0 (q)] if segment intersects triangle.
-    //! @return true if the segment intersects the triangle otherwise false.
+    #[inline]
+    #[allow(dead_code)]
+    pub unsafe fn new()->SegmentTriangleHitTester {
+        SegmentTriangleHitTester {
+            _p:Vector3::new(),
+            _pq:Vector3::new(),
+            _kx:0,
+            _ky:0,
+            _kz:0,
+            _sx:0.0,
+            _sy:0.0,
+            _sz:0.0
+
+        }
+    }
+
+    #[inline]
+    #[allow(dead_code)]
+    pub unsafe fn segment_triangle_hit_tester(p:&Vector3,q:&Vector3)->SegmentTriangleHitTester{
+       let mut result = SegmentTriangleHitTester::new();
+        result._p = p.to_owned();
+        result._pq = q-p;
+        let x_magnitude = result._pq.get_x().abs();
+        let y_magnitude = result._pq.get_y().abs();
+        let z_magnitude = result._pq.get_z().abs();
+
+
+        result._kz = 0;
+        result._kx = 1;
+        result._ky = 2;
+
+        if (z_magnitude >= y_magnitude)
+        {
+            if (z_magnitude >= x_magnitude)
+            {
+                result._kz = 2;
+                result._kx = 0;
+                result._ky = 1;
+            }
+        }
+        else
+        {
+            if (y_magnitude >= x_magnitude)
+            {
+                result._kz = 1;
+                result._kx = 2;
+                result._ky = 0;
+            }
+        }
+
+        if result._pq.get_element(result._kz) < 0.0
+        {
+            ptr::swap(result._kx.borrow_mut(),result._ky.borrow_mut());
+        }
+
+        result._sz = 1.0 / result._pq.get_element(result._kz);
+        result._sx = result._pq.get_element(result._kx) * result._sz;
+        result._sy = result._pq.get_element(result._ky) * result._sz;
+        result
+    }
+
+    #[inline]
+    #[allow(dead_code)]
+    pub unsafe fn IntersectSegmentTriangleCCW(a:){
+
+    }
     bool IntersectSegmentTriangleCCW(
     const AZ::Vector3& a, const AZ::Vector3& b, const AZ::Vector3& c, AZ::Vector3& normal, float& t) const
     {
@@ -56,20 +119,5 @@ private:
 template<bool oneSided>
 bool Intersect(const AZ::Vector3& a, const AZ::Vector3& b, const AZ::Vector3& c, AZ::Vector3& normal, float& t) const;
 
-AZ::Vector3 m_p;    //! Segment start point
-AZ::Vector3 m_pq;   //! Segment direction and length
 
-// This algorithm "rotates" the segment and triangle into a coordinate space where the largest component of the segment
-// is on the Z axis, while preserving a counter-clockwise winding. The following values are used to remap the XYZ dimensions
-// for this rotation.
-int m_kx; //! Which XYZ axis is "renamed" to the X dimension for the algorithm.
-int m_ky; //! Which XYZ axis is "renamed" to the Y dimension for the algorithm.
-int m_kz; //! Which XYZ axis is "renamed" to the Z dimension for the algorithm.
-
-// The segment and triangle are converted to a coordinate space where the segment is a unit ray of (0, 0, 1).
-// The following values are used to transform the triangles through translation, shear, and scale.
-float m_sx; //! Shear constant to use with the renamed X dimension
-float m_sy; //! Shear constant to use with the renamed Y dimension
-float m_sz; //! Shear constant to use with the renamed Z dimension
-};
 }
